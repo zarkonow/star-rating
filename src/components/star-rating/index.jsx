4e5489c9fd511051de8031f3cce53aa4bc3d0976
zarkonow <stojanov1987@gmail.com>
2024-10-21 20:08:14 +0200

    *added css Done

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -1,49 +1,38 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import "./styles.css";
 
+export default function StarRating({ numberOfStars = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
 
+  function handleClick(getCurrentIndex) {
+    setRating(getCurrentIndex);
+  }
 
-import React, { useState } from 'react'
-import { FaStar } from 'react-icons/fa'
-import './styles.css'
+  function handleMouseEnter(getCurrentIndex) {
+    setHover(getCurrentIndex);
+  }
 
-export default function StarRating({numberOfStars = 5}) {
-
-
-const [rating, setRating] = useState(0)
-const [hover, setHover] = useState(0)
-
-function handleClick(getCurrentIndex) {
-setRating(getCurrentIndex)
-
-}
-
-function handleMouseEnter(getCurrentIndex) {
-setHover(getCurrentIndex)
-
-}
-
-function handleMouseLeave(getCurrentIndex) {
-setHover(rating)
-
-}
-
+  function handleMouseLeave(getCurrentIndex) {
+    setHover(rating);
+  }
 
   return (
-    <div className='star-rating'>
-        {   
-                [...Array(numberOfStars)].map((_, index) => {
-                    index += 1
-                    return (
-                    <FaStar
-                    key = {index}
-                    className={index<=(hover || rating) ? 'active' : 'inactive'}
-                    onClick={() => handleClick(index)}
-                    onMouseMove={() => handleMouseEnter(index)}
-                    onMouseLeave={() => handleMouseLeave(index)}
-                    size={40}
-                    />
-                )
-                })
-            }
+    <div className="star-rating">
+      {[...Array(numberOfStars)].map((_, index) => {
+        index += 1;
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+            size={40}
+          />
+        );
+      })}
     </div>
-  )
+  );
 }
